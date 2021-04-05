53c2e023ebded359ea174a81f65e9725933bcd1f
František Harničár <frantisek.harnicar@student.tuke.sk>
2021-04-05 19:09:37 +0000

    Upload New File

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+import OpinionsHandlerMustache from "./OpinionsHandlerMustache.js";
+
+window.opnsHndlr = new OpinionsHandlerMustache("formular","opinionsContainer","mTmplOneOpinion");
+window.opnsHndlr.init();  
